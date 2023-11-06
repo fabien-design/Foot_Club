c38762373b152803aaaa2b3eafcb6f7aba73faf4
fabien-design <fabienrozier60@gmail.com>
2023-11-06 08:42:15 +0100

    ajout d'un commentaire

--- src/view/welcome.php
+++ src/view/welcome.php
@@ -10,7 +10,7 @@ $url = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 <ul>
     <li><a href="<?= Router::use("allPlayers") ?>">Liste des joueurs</a></li>
     <li><a href="<?= Router::use("allTeams") ?>">Liste des equipes</a></li>
-    <li><a href="<?= Router::use("allOppsClub") ?>">Liste des adversaires</a></li>
+    <!-- <li><a href="<?= Router::use("allOppsClub") ?>">Liste des adversaires</a></li> -->
     <li><a href="<?= Router::use("allMatches") ?>">Liste des match</a></li>
     <li><a href="<?= Router::use("allStaffs") ?>">Liste des membres du staff</a></li>
 </ul>
